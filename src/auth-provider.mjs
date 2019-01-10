dd121a21efe76f3f16309926943869dc4196263a
Markus Felten <Markus.Felten@gmx.de>
2019-01-11 00:18:05 +0100

    feat: only provide esm module
    BREAKING CHANGE: cjs module is gone

--- src/auth-provider.mjs
+++ src/auth-provider.mjs
@@ -1,3 +1,4 @@
+import keytar from "keytar";
 
 /**
  * @param {Object} options
@@ -10,12 +11,6 @@ export class KeyTarAuthProvider {
 
   async provideCredentials(realm) {
       const [account, service] = realm.split(/\//);
-
-      try {
-        const { getPassword } = require("keytar");
-        return { password: getPassword(account, service); }
-      } catch (e) {}
-      return undefined;
+      return { password: keytar.getPassword(account, service) };
     }
-  }
 }
--- tests/simple-test.mjs
+++ tests/simple-test.mjs
@@ -5,9 +5,7 @@ import { KeyTarAuthProvider } from "../src/auth-provider";
 test("has values", async t => {
   const authProvider = new KeyTarAuthProvider({ realmToUser:  { realmid: { user: "a", service: "b" }}});
 
-  const credentials = await authProvider.provideCredentials({
-    Basic: { realm: "realmid" }
-  });
+  const credentials = await authProvider.provideCredentials("a/realmid");
 
   t.is(credentials.user, "arlac77");
 });
